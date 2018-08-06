3c5c370ea7a3b8b6f35450712f873540815232ce
Christopher Darroch <christopher.darroch@gmail.com>
2018-08-06 21:16:50 +0200

    feat: detect indentation in package.json files when written to.

--- src/util/json.js
+++ src/util/json.js
@@ -1,11 +1,16 @@
 const readJson = require('load-json-file');
 const writeJson = require('write-json-file');
 
+const writeOpts = {
+    indent: 4,
+    detectIndent: true
+};
+
 function process(filename, process) {
     return new Promise((resolve, reject) => {
         readJson(filename)
             .then(process)
-            .then(json => writeJson(filename, json))
+            .then(json => writeJson(filename, json, writeOpts))
             .then(resolve, reject)
             .catch(reject);
     });
